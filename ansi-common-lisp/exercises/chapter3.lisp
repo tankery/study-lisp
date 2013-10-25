8422a6d94982e2a4c8ada6904b421bc39cc57046
Tankery Chen <tankery.chen@gmail.com>
2013-10-26 00:10:33 +0800

    Finished chapter 3.

--- ansi-common-lisp/exercises/chapter3.lisp
+++ ansi-common-lisp/exercises/chapter3.lisp
@@ -1,3 +1,4 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; 2.
 ; > (order-union '(a b c) '(b a d))
 ; (A B C D)
@@ -20,6 +21,7 @@
       lst
       (append lst (cons elm nil))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; 3.
 ; > (occurrences '(a b a d a c d c a))
 ; ((A . 4) (C . 2) (D . 2) (B . 1))
@@ -46,6 +48,7 @@
                   (setf result (cons new-unit result))))
             (occurrences-acc result (cdr lst))))))
           
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; 5.
 ; > (pos+ '(7 5 1 4))
 ; (7 6 3 7)
@@ -56,8 +59,29 @@
           (mapcar #'(lambda (x) (+ x 1))
                   (pos+ (cdr lst))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; 7. fewer cons cell compression.
+; > (compress '(a a a b b c d e e))
+(defun compress (x)
+  (if (consp x)
+    (compr (car x) 1 (cdr x))
+    x))
 
+(defun compr (elm n lst)
+  (if (null lst)
+    (list (n-elm n elm))
+    (let ((next (car lst)))
+      (if (eql elm next)
+        (compr elm (+ n 1) (cdr lst))
+        (cons (n-elm n elm) (compr next 1 (cdr lst)))))))
+
+(defun n-elm (n elm)
+  (if (> n 1)
+    ; this is the only difference......
+    (cons elm n)
+    elm))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; 8.
 ; > (showdots '(a b c))
 ; (A . (B . (C . NIL)))
@@ -69,3 +93,45 @@
         (format t "(~A . " (car lst))
         (showdots (cdr lst))
         (format t ")"))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 9. find the longest finite path through a network
+; > (longest-path 'a 'd '((a b c) (b c) (c d)))
+; (A B C D)
+(defun longest-path (start end net)
+  (bfs end (list (list start)) nil net))
+
+;; breadth-first search
+(defun bfs (end path-queue avail-path net)
+  (if (null path-queue)
+      ; all path has reached, return the last available path.
+      ; because we append path after the deeper node, we can easyly get the node from queue,
+      ; but this will make the path reversed.
+      (reverse avail-path)
+      ; get the first node and the path we get there
+      (let* ((path (car path-queue))
+             (node (car path)))
+        ; print the search process.
+        (format t " - ~A~%" path-queue)
+        (if (eql node end)
+            ; if found the destination, always use the path as result, the latest path will be the
+            ; longest path.
+            (bfs end (cdr path-queue) path net)
+            ; not found, search remaining queue.
+            (bfs end
+                 ; append new-paths to the end of queue, so the deeper node will be visit latter.
+                 (if (member node (cdr path))
+                     ; if node already in path, a loop occue, throw away this path.
+                     (cdr path-queue)
+                     ; node not in path, continue find next level node.
+                     (append (cdr path-queue)
+                             (new-paths path node net)))
+                 avail-path
+                 net)))))
+
+;; a new-path is a deeper level path.
+;; this function will find all node we can reach and cons with the path we get to there.
+(defun new-paths (path node net)
+  (mapcar #'(lambda (n)
+              (cons n path))
+          (cdr (assoc node net))))
